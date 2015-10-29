2df2015b1be64ddb2e69319d0f8adb7f9a080716
Gabriel Nakano <gtoshinakano@gmail.com>
2015-10-29 16:52:02 -0200

    Aula 1 alert, event e function

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<html>
+    <head>
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+        <meta charset="utf-8">
+
+        <title>Tutorial PHP Básico - 01 - HTML com Javascript</title>
+        
+        <!-- Essas 3 linhas carregam o Bootstrap já com Jquery  -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="page-header">
+                <h1>01 - HTML com Javascript</h1>
+            </div>
+            
+        </div>
+    </body>
+</html>            
